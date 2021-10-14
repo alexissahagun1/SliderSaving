e1574500d2c5bdcfb55fc6a100d8124d7a7b67a4
ITESM <ITESM>
2021-10-14 11:33:04 -0500

    Final changes and ready to deploy

--- ColorSlider/ContentView.swift
+++ ColorSlider/ContentView.swift
@@ -13,29 +13,39 @@ struct ContentView: View {
     @State private var red: Double = UserDefaults.standard.double(forKey: "red")
     @State private var blue: Double = UserDefaults.standard.double(forKey: "blue")
     @State private var green: Double = UserDefaults.standard.double(forKey: "green")
+    //
+    
+    //
+    var body: some View {
+        ZStack {
+            Color(red: red / 255, green: green / 255, blue: blue / 255)
+            
+            
+            VStack {
 //
 
-//
-    var body: some View {
-        
-        Text("\(red)")
-        Slider(value: $red, in: 0...255) {_ in
-            UserDefaults.standard.set(self.red, forKey: "red")
-        }
-        Text("\(blue)")
-        Slider(value: $blue, in: 0...255) {_ in
-            UserDefaults.standard.set(self.blue, forKey: "blue")
-        }
-        Text("\(green)")
-        Slider(value: $green, in: 0...255) {_ in
-            UserDefaults.standard.set(self.green, forKey: "green")
-            
+                Text("Red: \(red)")
+                
+                
+                Slider(value: $red, in: 0...255) {_ in
+                    UserDefaults.standard.set(self.red, forKey: "red")
+                }
+                Text("Blue: \(blue)")
+                Slider(value: $blue, in: 0...255) {_ in
+                    UserDefaults.standard.set(self.blue, forKey: "blue")
+                }
+                Text("Green: \(green)")
+                Slider(value: $green, in: 0...255) {_ in
+                    UserDefaults.standard.set(self.green, forKey: "green")
+                    
+                }
+            }.padding(.horizontal, 20)
         }
         
     }
     
 }
-    
+
 
 
 
